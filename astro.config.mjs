7feebee0cb3adb06c1738d6f419fbd860ca98394
Axi404 <axihelloworld@gmail.com>
2026-04-25 00:08:14 +0800

    chore: remove contributor page

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   redirects: {
     '/contributors': '/',
+    '/contributor': '/',
   },
   vite: {
     plugins: [tailwindcss()]
